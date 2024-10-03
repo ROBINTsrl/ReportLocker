8b555d8035f8f88f19147dc7c76e646815607bdb
Michele Santucci <weirdgyn@gmail.com>
2024-10-03 17:35:56 +0200

    First commit

---
+++ ReportLocker/Program.cs
@@ -0,0 +1,105 @@
+﻿using ReportLockerAPI;
+using System;
+using System.IO;
+
+namespace ReportLockerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage ReportLocker <--lock|--unlock|--check> <file|folder> [key]");
+                return;
+            }
+
+            if (args[0] == "--lock" && args.Length < 3)
+            {
+                Console.WriteLine("Use of --lock require a key value");
+                return;
+            }
+
+            string extension = Path.GetExtension(args[1]).ToLower();
+
+            DirectoryInfo dir = new DirectoryInfo(args[1]);
+
+            bool folder = dir.Exists;
+
+            if (!folder && !extension.Equals(".xlsx"))
+            {
+                Console.WriteLine("{0} file are not allowed (only xlsx)", extension);
+                return;
+            }
+
+            if (!folder && !File.Exists(args[1]))
+            {
+                Console.WriteLine("File {0} must exist", args[1]);
+                return;
+            }
+
+            ReportLocker reportLocker = new ReportLocker();
+
+            if (!folder)
+            {
+                if (args[0] == "--lock")
+                {
+                    if(reportLocker.Lock(args[1], args[2]))
+                        Console.WriteLine("Report {0} locked", args[1]);
+                    else
+                        Console.WriteLine("Report {0} lock failed", args[1]);
+                }
+                else if (args[0] == "--unlock")
+                {
+                    if(reportLocker.Unlock(args[1]))
+                        Console.WriteLine("Report {0} unlocked", args[1]);
+                    else
+                        Console.WriteLine("Report {0} unlock failed", args[1]);
+                }
+                else if (args[0] == "--check")
+                {
+                    ReportLocker.Protection result = reportLocker.GetProtection(args[1]);
+
+                    Console.WriteLine("Report {0} is {1}", args[1], result.ToString());
+                }
+            }
+            else 
+            {
+                string[] files = Directory.GetFiles(args[1], "*.xlsx");
+                int count = 0;
+
+                foreach (string file in files)
+                {
+                    if (args[0] == "--lock")
+                    {
+                        if (reportLocker.Lock(file, args[2]))
+                        {
+                            count++;
+                            Console.WriteLine("Report {0} locked", file);
+                        }
+                        else
+                            Console.WriteLine("Report {0} lock failed", file);
+                    }
+                    else if (args[0] == "--unlock")
+                    {
+                        if (reportLocker.Unlock(file))
+                        {
+                            count++;
+                            Console.WriteLine("Report {0} unlocked", file);
+                        }
+                        else
+                            Console.WriteLine("Report {0} unlock failed", file);
+                    }
+                    else if (args[0] == "--check")
+                    {
+                        ReportLocker.Protection result = reportLocker.GetProtection(file);
+
+                        Console.WriteLine("Report {0} is {1}", file, result.ToString());
+                    }
+                }
+
+                Console.WriteLine("{0} files {1} processed ", files.Length, count);
+            }
+        }
+    }
+}
---
+++ ReportLockerAPI/ReportLocker.cs
@@ -0,0 +1,144 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Linq;
+
+namespace ReportLockerAPI
+{
+    public class ReportLocker
+    {
+        public enum Protection {
+            Unlocked = 0,
+            Locked = 1,
+            Crypted = 2
+        }
+
+        public ReportLocker()
+        {
+        }
+
+        protected string HexPasswordConversion(string password)
+        {
+            byte[] passwordCharacters = System.Text.Encoding.ASCII.GetBytes(password);
+            
+            int hash = 0;
+
+            if (passwordCharacters.Length > 0)
+            {
+                int charIndex = passwordCharacters.Length;
+
+                while (charIndex-- > 0)
+                {
+                    hash = ((hash >> 14) & 0x01) | ((hash << 1) & 0x7fff);
+                    hash ^= passwordCharacters[charIndex];
+                }
+
+                // Main difference from spec, also hash with charcount
+                hash = ((hash >> 14) & 0x01) | ((hash << 1) & 0x7fff);
+                hash ^= passwordCharacters.Length;
+                hash ^= (0x8000 | ('N' << 8) | 'K');
+            }
+
+            return Convert.ToString(hash, 16).ToUpperInvariant();
+        }
+        public bool Lock(string report, string key)
+        {
+            if (string.IsNullOrEmpty(report))
+                throw new ArgumentException("Report file required");
+
+            if (!System.IO.File.Exists(report))
+                throw new ArgumentException("Report file must exist");
+
+            string hexKey = HexPasswordConversion(key);
+
+            try
+            {
+                using (SpreadsheetDocument document = SpreadsheetDocument.Open(report, true))
+                {
+                    foreach (var part in document.WorkbookPart.WorksheetParts)
+                    {
+                        SheetProtection protection = new SheetProtection()
+                        {
+                            Sheet = true,
+                            Objects = true,
+                            Scenarios = true,
+                            Password = hexKey
+                        };
+
+                        part.Worksheet.InsertAfter(protection, part.Worksheet.Descendants<SheetData>().LastOrDefault());
+                        part.Worksheet.Save();
+                    }
+                }
+            }
+            catch (System.IO.FileFormatException)
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return true;
+        }
+        public bool Unlock(string report)
+        {
+            if (string.IsNullOrEmpty(report))
+                throw new ArgumentException("Report file required");
+
+            if (!System.IO.File.Exists(report))
+                throw new ArgumentException("Report file must exist");
+
+            try
+            {
+                using (SpreadsheetDocument document = SpreadsheetDocument.Open(report, true))
+                {
+                    foreach (var part in document.WorkbookPart.WorksheetParts)
+                    {
+                        part.Worksheet.RemoveAllChildren<SheetProtection>();
+                        part.Worksheet.Save();
+                    }
+                }
+            }
+            catch (System.IO.FileFormatException)
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return true;
+        }
+        public Protection GetProtection(string report)
+        {
+            if (string.IsNullOrEmpty(report))
+                throw new ArgumentException("Report file required");
+
+            if (!System.IO.File.Exists(report))
+                throw new ArgumentException("Report file must exist");
+
+            try
+            {
+                using (SpreadsheetDocument document = SpreadsheetDocument.Open(report, false))
+                {
+                    foreach (var part in document.WorkbookPart.WorksheetParts)
+                    {
+                        var protections = part.Worksheet.Elements<SheetProtection>();
+
+                        if (protections.Any())
+                            return Protection.Locked;
+                    }
+                }
+            }
+            catch (System.IO.FileFormatException) {
+                return Protection.Crypted;
+            }
+
+            return Protection.Unlocked;
+        }
+
+        public static ReportLocker Create() { return new ReportLocker(); }
+    }
+}
